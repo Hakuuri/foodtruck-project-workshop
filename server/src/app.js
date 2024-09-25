36b12e349d09c544008669e2946af257add10c30
Hakuuri <enzo.abancourt@ecoles-epsi.net>
2024-09-25 20:07:36 +0200

    create temporary database

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const sql = require('mssql');  
+const fs = require('fs');
+const path = require('path');
 
 
 const app = express();
@@ -10,24 +12,27 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-const dbConfig = {
-    user: 'sql_username',     
-    password: 'sql_password', 
-    server: 'server_address', 
-    database: 'database_name',
-    options: {
-      encrypt: true,               
-      trustServerCertificate: true 
-    }
-  };
+
+// SQL 
+
+// const dbConfig = {
+//     user: 'sql_username',     
+//     password: 'sql_password', 
+//     server: 'server_address', 
+//     database: 'database_name',
+//     options: {
+//       encrypt: true,               
+//       trustServerCertificate: true 
+//     }
+//   };
 
 
-  // SQL Server
-sql.connect(dbConfig).then(() => {
-    console.log('Connected to SQL Server');
-  }).catch((err) => {
-    console.error('Failed to connect to SQL Server', err);
-  });
+//   // SQL Server
+// sql.connect(dbConfig).then(() => {
+//     console.log('Connected to SQL Server');
+//   }).catch((err) => {
+//     console.error('Failed to connect to SQL Server', err);
+//   });
 
 // create/register a user
 app.post('/register', async (req, res) => {
@@ -35,10 +40,10 @@ app.post('/register', async (req, res) => {
       const { username, email, password, phone, birthDate } = req.body;
       
       // SQL query to insert user data into the database
-      const query = `
-        INSERT INTO Users (username, email, password, phone, birthDate) 
-        VALUES (@username, @email, @password, @phone, @birthDate)
-      `;
+    //   const query = `
+    //     INSERT INTO Users (username, email, password, phone, birthDate) 
+    //     VALUES (@username, @email, @password, @phone, @birthDate)
+    //   `;
   
       // Create a new SQL request and execute the query
       const request = new sql.Request();
@@ -47,12 +52,12 @@ app.post('/register', async (req, res) => {
       request.input('password', sql.VarChar, password);
       request.input('phone', sql.VarChar, phone);
       request.input('birthDate', sql.Date, birthDate);
-      
-      await request.query(query);
+      return res.send();   
+    //   await request.query(query);
   
-      res.send({
-        message: `User ${email} successfully registered`
-      });
+    //   res.send({
+    //     message: `User ${email} successfully registered`
+    //   });
     } catch (error) {
       res.status(500).send({
         message: 'Error during registration',
@@ -62,10 +67,23 @@ app.post('/register', async (req, res) => {
   });
 
 
-// app.post('/register', (req, res) => {
-//     res.send({
-//         message: `hello ${req.body.email} the user succesfully registered`
-//     })
-// })
+
+  //json 
+
+  const dataFilePath = path.join(__dirname, '../BDD.json');
+
+// Helper function to read data from the JSON file
+const readDataFromFile = () => {
+    try {
+        const data = fs.readFileSync(dataFilePath, 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        // If file does not exist, return an empty array
+        return [];
+    }
+};
+
+
+
 
 app.listen(process.env.PORT || 8081 )
